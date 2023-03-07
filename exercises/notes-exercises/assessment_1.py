37196a3973752dd430c5b5f51ece4bcdddcb1c91
Tresor <tresor.twendi@gmail.com>
2023-03-07 01:46:18 +0000

    Updated assessment_1.py

--- exercises/notes-exercises/assessment_1.py
+++ exercises/notes-exercises/assessment_1.py
@@ -9,15 +9,15 @@ print('**********Card Busters**********')
 for i in range(7):
     turn += 1
     if player_one_cards[i] > player_two_cards[i]:
-        print('Round number', str(turn) + ':', 'Player 1 wins the round, with', player_one_cards[i], ' beating ',
+        print('Round number', str(turn) + ': Player 1 wins the round, with', player_one_cards[i], ' beating ',
               player_two_cards[i])
         player_one_score += 1
     elif player_one_cards[i] < player_two_cards[i]:
-        print('Round number', str(turn) + ':', 'Player 2 wins the round, with', player_two_cards[i], ' beating ',
+        print('Round number', str(turn) + ': Player 2 wins the round, with', player_two_cards[i], ' beating ',
               player_one_cards[i])
         player_two_score += 1
     else:
-        print('Round number', str(turn) + ':', 'This round has ended in a draw')
+        print('Round number', str(turn) + ': This round has ended in a draw')
 
 print()
 if player_one_score > player_two_score:
